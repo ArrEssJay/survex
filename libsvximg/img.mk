a86a2fcd68123667db708d0b389d58fd7432dfac
Robert Jones <rob.jones@dolby.com>
2013-10-12 02:43:22 +1100

    Remove unnecessary python include

--- libsvximg/img.mk
+++ libsvximg/img.mk
@@ -1,7 +1,7 @@
 _img.so: img_wrap.o
 	gcc -dynamiclib -lpython *.o -o _img.so
 img_wrap.o: img_wrap.c
-	gcc -fPIC img_wrap.c  -o img_wrap.o -I/usr/include/python2.7/ -L/usr/lib/python2.7 -c
+	gcc -fPIC img_wrap.c  -o img_wrap.o -I/usr/include/python2.7/ -c
 img_wrap.c: img.c img.i
 	swig -python -o img_wrap.c img.i
 clean:
