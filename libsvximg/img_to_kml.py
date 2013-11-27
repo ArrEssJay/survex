8e21c168904c985ca7e8de6576cccdd0ba14099f
Robert Jones <rob.jones@dolby.com>
2013-11-27 22:34:01 +1100

    Add extra command line options Improve comments Refactor image parsing General tidyup

--- libsvximg/img_to_kml.py
+++ libsvximg/img_to_kml.py
@@ -9,12 +9,13 @@ from lxml import etree
 from vec3 import *
 from math import *
 
-#config
+#default config
 epsg="epsg:28356" #GDA94 (UTM)
-refstation = "ent.j13"
-ref_E = 0224154.45
-ref_N = 6255362.60
-ref_Z = 842.48
+geod_sphere="GRS80"
+#refstation = "ent.j13"
+#ref_E = 0224154.45
+#ref_N = 6255362.60
+#ref_Z = 842.48
 alt_offset = 300
 alt_exag = 1
 
@@ -28,72 +29,12 @@ def copy_point(p):
     pt.z = p.z
     return pt
 
-class XSect:
-    "store cross section data"
-    def __init__(self, l=None, r=None, u=None, d=None, station=None):
-        self.l = l
-        self.r = r
-        self.u = u
-        self.d = d
-        self.station = station
-
-class Tpos:
-    INVALID = -1
-    FIRST = 0
-    INTER = 1
-    LAST = 2
-
-def ptocart(p, rs, rLon, rLat, geod):
-    dx = p.x - rs.x
-    dy = p.y - rs.y
-
-    #for altitude, calculate the delta from the reference station
-    #then add to the value at the reference mark
-    dz = p.z - rs.z
-    sAlt = ref_Z + dz
-    #print 'dx=%8.2f, dy=%8.2f, dz=%8.2f' % (dx,dy,dz)
-
-    #avoid divide by zero
-    #if dx == 0:
-    #    phi = 0
-    #else:
-    phi = degrees(atan2(dx, dy))
-        #phi = math.degrees(math.atan(dy/dx))
-         #wrt x plane
-      #  if dx>=0 and dy>=0:
-      #      phi = 90 - phi
-      #  if dx>=0 and dy<0:
-      #      phi = phi + 90
-      #  if dx<0 and dy<0:
-      #      phi = 270 - phi
-      #  if dx<0 and dy>=0:
-      #      phi = 360 - phi
-
-    #r = math.sqrt(pow(dx,2) + pow(dy,2))
-    r = hypot(dx, dy)
-    #print"r=%8.2f, phi=%8.2f" % (r,phi)
-
-    sLon, sLat, sBAz = geod.fwd(rLon, rLat, phi, r, radians=False)
-    #print"sLon = %8.2f, sLat = %8.2f, sBAz = %8.2f, sAlt = %8.2f" % (sLon, sLat, sBAz, sAlt)
-
-    return sLon, sLat, (sAlt*alt_exag)+alt_offset
-
-def main(argv=None):
-    global dryrun, thexec, adbexec, waitfordevice
-    if argv is None:
-        argv = sys.argv
-
-    print argv
-
-    parser = argparse.ArgumentParser(description='Dump Contents of Survex .3d file as text')
-    parser.add_argument('-i', '--input', help='Input 3D File', required=True)
-
-    shell_args, unparsed_args = parser.parse_known_args()
-
-    print shell_args.input
+def parse_3d(infile):
+    "Parse a .3D file to lists of stations, cross sections and traverses"
     survey = ''
-    pimg = img_open_survey(shell_args.input, survey)
+    pimg = img_open_survey(infile, survey)
 
+    #print the .3d header
     if(pimg):
         print('File: '+ shell_args.input)
         print('------\nHeader\n------')
@@ -107,19 +48,18 @@ def main(argv=None):
         print('Failed to open: ' + shell_args.input)
         return 1
 
-    #Parse the file and get lists of traverses
-
-
+    #prime lists of stations, centreline traverses, tube traverses
     stations = {}
     traverses = []
     tubes = []
 
+    #prime lists of stations in current tube/centreline traverses
     cXSTrav = []
     cLTrav = []
 
 
     #current traverse/tube state
-    #cTrav is only to check if we did a line before a move
+    #cTrav is used to check that we didn't attempt to start a line before a move
     cTrav = cTube = False
 
     p = img_point()
@@ -145,8 +85,8 @@ def main(argv=None):
 
         elif (item == img_XSECT_END):
             if(cTube == False):
-                print("Error: not in a tube to end")
-                return 1
+                print("ERROR: not in a tube to end")
+                #return 1
             tubes.append(cXSTrav)
             print("Added a tube with %d cross sections", len(cXSTrav))
             cTube = False
@@ -165,8 +105,8 @@ def main(argv=None):
 
         elif (item == img_LINE):
             if(cTrav == False):
-                print("Line before move")
-                return 1
+                print("ERROR: Line before move")
+                #return 1
             pt = copy_point(p)
             cLTrav.append(pt)
 
@@ -191,12 +131,73 @@ def main(argv=None):
     #print stations.items()
     #for k,v in stations.items():
     #    print ("%s : %8.2f %8.2f %8.2f") % (k,v.x, v.y, v.z)
+    return traverses, tubes, stations
 
 
+class XSect:
+    "store cross section data"
+    def __init__(self, l=None, r=None, u=None, d=None, station=None):
+        self.l = l
+        self.r = r
+        self.u = u
+        self.d = d
+        self.station = station
 
-        #find the reference station
+class Tpos:
+    INVALID = -1
+    FIRST = 0
+    INTER = 1
+    LAST = 2
 
-    print "Refstation: %s" %(refstation)
+def ptocart(p, rs, rLon, rLat, geod):
+    "Given a reference station with local co-ordinates rs at location rLon, rLat, calculate Lat and Lon for a station with local co-ordinates p"
+
+    #Calculate x,y distance of point from reference
+    dx = p.x - rs.x
+    dy = p.y - rs.y
+
+    #for altitude, add the delta from the reference station's elevation
+    #to the reference station's altitude
+    dz = p.z - rs.z
+    sAlt = ref_Z + dz
+
+    #convert cartesian dx, dy to polar r, phi
+    phi = degrees(atan2(dx, dy))
+    r = hypot(dx, dy)
+
+    sLon, sLat, sBAz = geod.fwd(rLon, rLat, phi, r, radians=False)
+
+    return sLon, sLat, (sAlt*alt_exag)+alt_offset
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    parser = argparse.ArgumentParser(description='Dump Contents of Survex .3d file as text')
+    parser.add_argument('-i', '--input', help='Input 3D File', required=True)
+    parser.add_argument('-r', '--ref_station', help='Station to georeference model to', required=True)
+    parser.add_argument('-rs', '--ref_station', help='Station name to georeference model to', required=True)
+    parser.add_argument('-re', '--ref_east', help='Reference station location Easting', required=True)
+    parser.add_argument('-rn', '--ref_north', help='Reference station location Northing', required=True)
+    parser.add_argument('-ra', '--ref_alt', help='Reference station altitude (m)', required=True)
+    parser.add_argument('-d', '--datum', help='Datum string to supply to Proj for georeferncing. Defaults to "epsg:28356" (#GDA94 (UTM))', required=False)
+    parser.add_argument('-g', '--datum', help='S', required=False)
+
+
+    parser.add_argument('-o', '--output', help='Output File', required=False)
+    parser.add_argument('-z', '--zip', help='zip contents to (.KMZ); automatically true for Collada output', required=False)
+    parser.add_argument('-c', '--collada', help='output geometry in Collada .dae format', required=False)
+
+
+    shell_args, unparsed_args = parser.parse_known_args()
+
+    #print shell_args.input
+
+    traverses, tubes, stations = parse_3d(shell_args.input)
+
+    #locate the reference station
+    refstation = shell_args.ref_station
+    print "Reference Station Name: %s" %(refstation)
     if stations.has_key(refstation):
         print ("Reference station found")
         rs =stations.get(refstation)
@@ -209,13 +210,13 @@ def main(argv=None):
     #Calculate distances of all stations from reference station
 
     #calculate absolute position of reference station
-    p = Proj(init=epsg)
-    rLon, rLat = p(ref_E, ref_N,inverse=True)
+    pProj = Proj(init=epsg)
+    rLon, rLat = pProj(ref_E, ref_N,inverse=True)
 
     print "Reference Station Lat: %8.2f degrees, Lon: %8.2f degrees" % (rLat, rLon)
 
     #setup Geod
-    geod = Geod(ellps='GRS80')
+    geod = Geod(ellps=geod_sphere)
 
     #open KML
     #create KML object
